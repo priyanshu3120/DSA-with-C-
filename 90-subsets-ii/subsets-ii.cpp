cee960aff3c0029335f191ab3a1dfbe2205ca040
priyanshu3120 <priyanshumaurya8721@gmail.com>
2025-07-29 00:15:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (67.24%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    void findSubsets(int index, vector<int> &nums, vector<int> &ds, vector<vector<int>> &ans) {
-          if(index == nums.size())
+    void findSubsets(int i, vector<int> &nums, vector<int> &ds, vector<vector<int>> &ans) {
+          if(i == nums.size())
           {
              ans.push_back(ds);
              return;
           }
           
-          ds.push_back(nums[index]);
-          findSubsets(index+1,nums,ds,ans);
+          ds.push_back(nums[i]);
+          findSubsets(i+1,nums,ds,ans);
           ds.pop_back();
 
-          int idx=index+1;
+          int idx=i+1;
           while(idx < nums.size() && nums[idx] == nums[idx-1])
           {
             idx++;
