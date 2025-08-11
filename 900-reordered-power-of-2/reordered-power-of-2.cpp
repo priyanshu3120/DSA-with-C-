f50005d28af58d6cf5db8644907cc31af677915f
priyanshu3120 <priyanshumaurya8721@gmail.com>
2025-08-11 16:33:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.62%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    string getSortedStr(int n) {
-        string s = to_string(n);
-        sort(begin(s), end(s));
+    string sortedDigit(int n)
+    {
+        string s=to_string(n);
+        sort(s.begin(),s.end());
         return s;
     }
-
     bool reorderedPowerOf2(int n) {
-        string s = getSortedStr(n);
+        string s=sortedDigit(n);
 
-        //check all 2 powers and see if it matches with s
-        //2^29
-        for(int p = 0; p <= 29; p++) {
-            if(s == getSortedStr(1 << p)) {
+        for(int i=0; i<=29; i++)
+        {
+            if(s == sortedDigit(pow(2,i))) // instead of pow(2,i) we also use (1 << i)
+            {
                 return true;
             }
         }
-
         return false;
     }
 };
